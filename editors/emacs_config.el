7601b26aebd76636f8669499d5b2bbce4c9d7052
TheWatermelon <bastien.chapusot@gmx.com>
2019-03-09 17:26:41 +0100

    removed some distraction-free utilities in emacs

--- editors/emacs_config.el
+++ editors/emacs_config.el
@@ -95,22 +95,7 @@
                         '(("^ *\\([-]\\) "
                            (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 ;; Long lines will flow and adjust to the width of the window
-;(add-hook 'org-mode-hook 'visual-line-mode)
-
-;;;             ;;;
-;; Markdown mode ;;
-;;;             ;;;
-;;(use-package markdown-mode
-;;  :ensure t
-;;  :commands (markdown-mode gfm-mode)
-;;  :mode (("README\\.md\\'" . gfm-mode)
-;;         ("\\.md\\'" . markdown-mode)
-;;         ("\\.markdown\\'" . markdown-mode))
-;;  :init (setq markdown-command "multimarkdown"))
-;; Use visual-line-mode in gfm-mode
-;;(defun my-gfm-mode-hook ()
-;;  (visual-line-mode 1))
-;;(add-hook 'gfm-mode-hook 'my-gfm-mode-hook)
+(add-hook 'org-mode-hook 'visual-line-mode)
 
 ;;;                      ;;;
 ;; Distraction-free setup ;;
@@ -133,43 +118,56 @@
 (setq inhibit-startup-echo-area-message "walter")
 
 ;; See http://bzg.fr/emacs-hide-mode-line.html
-(defvar-local hidden-mode-line-mode nil)
-(defvar-local hide-mode-line nil)
+;;(defvar-local hidden-mode-line-mode nil)
+;;(defvar-local hide-mode-line nil)
 
-(define-minor-mode hidden-mode-line-mode
-  "Minor mode to hide the mode-line in the current buffer."
-  :init-value nil
-  :global nil
-  :variable hidden-mode-line-mode
-  :group 'editing-basics
-  (if hidden-mode-line-mode
-      (setq hide-mode-line mode-line-format
-            mode-line-format nil)
-    (setq mode-line-format hide-mode-line
-          hide-mode-line nil))
-  (force-mode-line-update)
-  ;; Apparently force-mode-line-update is not always enough to
-  ;; redisplay the mode-line
-  (redraw-display)
-  (when (and (called-interactively-p 'interactive)
-             hidden-mode-line-mode)
-    (run-with-idle-timer
-     0 nil 'message
-     (concat "Hidden Mode Line Mode enabled.  "
-             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+;;(define-minor-mode hidden-mode-line-mode
+;;  "Minor mode to hide the mode-line in the current buffer."
+;;  :init-value nil
+;;  :global nil
+;;  :variable hidden-mode-line-mode
+;;  :group 'editing-basics
+;;  (if hidden-mode-line-mode
+;;      (setq hide-mode-line mode-line-format
+;;            mode-line-format nil)
+;;    (setq mode-line-format hide-mode-line
+;;          hide-mode-line nil))
+;;  (force-mode-line-update)
+;;  ;; Apparently force-mode-line-update is not always enough to
+;;  ;; redisplay the mode-line
+;;  (redraw-display)
+;;  (when (and (called-interactively-p 'interactive)
+;;             hidden-mode-line-mode)
+;;    (run-with-idle-timer
+;;     0 nil 'message
+;;     (concat "Hidden Mode Line Mode enabled.  "
+;;             "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
 
 ;; Activate hidden-mode-line-mode
-(hidden-mode-line-mode 1)
+;;(hidden-mode-line-mode 1)
+
+;; Command to toggle the display of the mode-line as a header
+;;(defvar-local header-line-format nil)
+;;(defun mode-line-in-header ()
+;;  (interactive)
+;;  (if (not header-line-format)
+;;      (setq header-line-format mode-line-format
+;;            mode-line-format nil)
+;;    (setq mode-line-format header-line-format
+;;          header-line-format nil))
+;;  (set-window-buffer nil (current-buffer)))
+;;(global-set-key (kbd "C-s-SPC") 'mode-line-in-header)
 
 ;; If you want to hide the mode-line in all new buffers
-(add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
+;;(add-hook 'after-change-major-mode-hook 'hidden-mode-line-mode)
 
 ;; Alternatively, you can paint your mode-line in White but then
 ;; you'll have to manually paint it in black again
-;; (custom-set-faces
-;;  '(mode-line-highlight ((t nil)))
+(custom-set-faces
+ '(mode-line-highlight ((t nil)))
 ;;  '(mode-line ((t (:foreground "white" :background "white"))))
-;;  '(mode-line-inactive ((t (:background "white" :foreground "white")))))
+ ;;  '(mode-line-inactive ((t (:background "white" :foreground "white"))))
+)
 
 ;; A small minor mode to use a big fringe
 (defvar bzg-big-fringe-mode nil)
